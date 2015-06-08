d1e8583a726ed0aacb011db8a9ac26343c8f9392
Antonio Tenorio Fornés <antoniotenorio@ucm.es>
2015-06-08 16:03:27 +0200

    minor: code style

--- angular-swellrt.js
+++ angular-swellrt.js
@@ -72,13 +72,13 @@ angular.module('SwellRTService',[])
            defSession.reject('Error conecting to wavejs server: Try again later');
          });
        });
-    };
+    }
 
     function stopSession(server, user, pass){
        swellrt.then(function() {
          window.SwellRT.stopSession();
        });
-    };
+    }
 
     function openModel(waveId){
       var deferred = $q.defer();
